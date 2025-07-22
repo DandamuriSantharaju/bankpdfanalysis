77820db9b665575e8385cde1d9dbe2ee58853eb6
DandamuriSantharaju <rajusantha040@gmail.com>
2025-07-22 12:10:59 +0530

    Adding localhost url

--- backend/settings.py
+++ backend/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
         },
     },
 ]
-CORS_ALLOWED_ORIGINS = ["https://banpdfanalysis.netlify.app"]
+CORS_ALLOWED_ORIGINS = ["https://banpdfanalysis.netlify.app","http://localhost:4200/"]
 
 CORS_ALLOW_HEADERS = list(default_headers) + ["content-disposition"]
 CORS_EXPOSE_HEADERS = [
